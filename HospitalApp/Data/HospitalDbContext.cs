77acf0e4b06213902a55d9bf3dc7d406602dafb7
71/* <akifst@JUPITER08>
2024-08-08 21:26:41 +0400

    Add project files.

---
+++ HospitalApp/Data/HospitalDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HospitalApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalApp.Data
+{
+    public class HospitalDbContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        public HospitalDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ HospitalApp/Entities/Department.cs
@@ -0,0 +1,10 @@
+﻿namespace HospitalApp.Entities
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Limit { get; set; }
+        public string Image { get; set; }
+    }
+}
